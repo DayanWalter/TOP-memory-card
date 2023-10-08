8dd0c2dd1ebb5edab6d3c08fbb522ec6d7d4e220
SynthCyraX <SynthCyraX@gmail.com>
2023-10-08 09:46:32 +0200

    Added cards and pokemon component

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,21 @@
 import { useState, useEffect } from 'react';
+import Pokemon from './Pokemon';
+import Card from './Card';
 
 export default function App() {
-  const [pokemon, setPokemon] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const cards = [37, 39, 77, 121, 133];
 
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/1/');
-        if (!response.ok) {
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-          );
-        }
-
-        let actualData = await response.json();
-
-        setPokemon(actualData);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setPokemon(null);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchUserData();
-  }, []);
   return (
     <>
       <h1>Pokemon Memory</h1>
-      {loading && <div>A moment please...</div>}
-      {error && <div>{`There is a problem fetching the data - ${error}`}</div>}
-      {pokemon && (
-        <img src={pokemon.sprites.other.dream_world.front_default} alt="" />
-      )}
+
+      <>
+        {cards.map((card) => (
+          <Card key={card}>
+            <Pokemon key={card} id={card} />
+          </Card>
+        ))}
+      </>
     </>
   );
 }
---
+++ src/Pokemon.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+
+export default function Pokemon({ id }) {
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  console.log(id);
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${id}/`
+        );
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+
+        let actualData = await response.json();
+
+        setPokemon(actualData);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setPokemon(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPokemonData();
+  }, []);
+
+  function firstLetterUpperCase(name) {
+    return name[0].toUpperCase() + name.slice(1);
+  }
+
+  return (
+    <>
+      {console.log(id)}
+      {loading && <div>A moment please...</div>}
+      {error && <div>{`There is a problem fetching the data - ${error}`}</div>}
+      {pokemon && (
+        <>
+          <img
+            key={id}
+            src={pokemon.sprites.other.dream_world.front_default}
+            alt={pokemon.id}
+          />
+          <p>{firstLetterUpperCase(pokemon.name)}</p>
+        </>
+      )}
+    </>
+  );
+}
