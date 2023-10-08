5b5ab36e9cf6649ba6bd83bf9d3542ba7479c86a
SynthCyraX <SynthCyraX@gmail.com>
2023-10-08 06:38:25 +0200

    Fetched and displayed data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/1/');
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+
+        let actualData = await response.json();
+
+        setPokemon(actualData);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setPokemon(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUserData();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Pokemon Memory</h1>
+      {loading && <div>A moment please...</div>}
+      {error && <div>{`There is a problem fetching the data - ${error}`}</div>}
+      {pokemon && (
+        <img src={pokemon.sprites.other.dream_world.front_default} alt="" />
+      )}
     </>
-  )
+  );
 }
-
-export default App
